0eaa8fba469cc4a85f0f51a7d4e7f2002f838bc1
tanoss <31858947+tanoss@users.noreply.github.com>
2018-01-21 19:47:08 -0500

    del1
    del1

---
+++ inteligencia.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+        vector =  [4,5,6] 
+    norma = calcularNorma(vector);
+    ESCRIBIRrESULTADO(norma)
+        });
+
+    
+   function ESCRIBIRrESULTADO(resultado){
+       document.getElementById("resultado").innerHTML=resultado;
+
+   }
+   function calcularNorma(vector){
+       norma = 0;
+       for(i=0;i < vector.length ; i++) {
+           norma = norma+(vector[i]+vector[i])
+       }
+       norma = Math.sqrt(norma)
+       return norma;
+   }
---
+++ programa/Sis Biblioteca v01/dbconexion.php
@@ -0,0 +1,5 @@
+<?php
+$cnmysql = new mysqli("localhost","root","12345","dbbiblioteca");
+//$cnmysql = new mysqli("sql313.byethost32.com","b32_21140731","arbol123","b32_21140731_dbbiblioteca");
+
+?>
